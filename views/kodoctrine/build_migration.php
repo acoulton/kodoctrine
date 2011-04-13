5983b9ab9850e0b97911b65bb59c5f0c83f85d57
Andrew Coulton <andrewnfcoulton@gmail.com>
2011-04-13 22:14:08 +0100

    Improve build migration view

--- views/kodoctrine/build_migration.php
+++ views/kodoctrine/build_migration.php
@@ -3,53 +3,167 @@
 <h2>Build Database Migrations</h2>
 <p>Your version: <?=$migration->getCurrentVersion();?></p>
 <p>Latest version: <?=$migration->getLatestVersion();?></p>
-<?php foreach ($changes as $schema=>$change_types):?>
-<h3><?=$schema?></h3>
 <table>
-    <?php foreach ($change_types as $type=>$details):
-        if (count($details)):
-                switch ($type) {
-                    case 'created_tables':
-                    case 'created_foreign_keys':
-                    case 'created_columns':
-                        $rowclass='created';
-                        break;
-                    case 'changed_columns':
-                        $rowclass='changed';
-                        break;
-                    default:
-                        $rowclass='dropped';
-                }
-                $rowspan=count($details);
-            else:
-                $rowclass='inactive';
-                $rowspan=1;
-            endif;?>
-            <tr class="<?=$rowclass?>">
-                <td rowspan="<?=$rowspan?>"><?=$type?></td>
-                <?php foreach ($details as $data):
-                    switch ($type)
-                {
-                    case 'created_tables':
-                    case 'dropped_tables':
-                        $key = $data['tableName'];
-                        break;
-                    default:
-                        if (is_array($data)) {
-                            $key = key($data);
-                        }
-                        else
-                        {
-                            $key = $data;
-                        }
-                }?>
-                <td><?=$key?></td>
-                </tr><tr>
-                <?php endforeach;?>
-            </tr>
-    <?php endforeach;?>
+    <tbody class='dropped'>
+        <tr><th colspan="5">Dropped Tables</th></tr>
+        <?php
+            $details = Arr::get($changes, 'dropped_tables', array());
+            foreach ($details as $table=>$columns):
+                $first = true;
+                foreach ($columns as $name=>$details):?>
+                <tr>
+                    <?php if ($first):?>
+                        <td rowspan ="<?=count($columns);?>"><?=$table;?></td>
+                    <?php $first = false; endif;?>
+                        <td><?=$name?></td>
+                        <td><?=$details['type']?></td>
+                        <td><?=$details['length']?></td>
+                        <td></td>
+                </tr>
+          <?php endforeach;
+            endforeach;?>
+        <tr><th colspan="5">Dropped Columns</th></tr>
+        <?php
+            $details = Arr::get($changes, 'dropped_columns', array());
+            foreach ($details as $table=>$columns):
+                $first = true;
+                foreach ($columns as $name=>$details):?>
+                <tr>
+                    <?php if ($first):?>
+                        <td rowspan ="<?=count($columns);?>"><?=$table;?></td>
+                    <?php $first = false; endif;?>
+                        <td><?=$name?></td>
+                        <td><?=$details['type']?></td>
+                        <td><?=$details['length']?></td>
+                        <td></td>
+                </tr>
+          <?php endforeach;
+            endforeach;?>
+        <tr><th colspan="5">Dropped Foreign Keys</th></tr>
+        <?php
+            $details = Arr::get($changes, 'dropped_foreign_keys', array());
+            foreach ($details as $table=>$columns):
+                $first = true;
+                foreach ($columns as $name=>$details):?>
+                <tr>
+                    <?php if ($first):?>
+                        <td rowspan ="<?=count($columns);?>"><?=$table;?></td>
+                    <?php $first = false; endif;?>
+                        <td><?=$name?></td>
+                        <td><?=$details['type']?></td>
+                        <td><?=$details['length']?></td>
+                        <td></td>
+                </tr>
+          <?php endforeach;
+            endforeach;?>
+        <tr><th colspan="5">Dropped Indexes</th></tr>
+        <?php
+            $details = Arr::get($changes, 'dropped_indexes', array());
+            foreach ($details as $table=>$columns):
+                $first = true;
+                foreach ($columns as $name=>$details):?>
+                <tr>
+                    <?php if ($first):?>
+                        <td rowspan ="<?=count($columns);?>"><?=$table;?></td>
+                    <?php $first = false; endif;?>
+                        <td><?=$name?></td>
+                        <td><?=$details['type']?></td>
+                        <td><?=$details['length']?></td>
+                        <td></td>
+                </tr>
+          <?php endforeach;
+            endforeach;?>
+    </tbody>
+    <tbody class='changed'>
+        <tr><th colspan="5">Changed Columns</th></tr>
+        <?php
+            $details = Arr::get($changes, 'changed_columns', array());
+            foreach ($details as $table=>$columns):
+                $first = true;
+                foreach ($columns as $name=>$details):?>
+                <tr>
+                    <?php if ($first):?>
+                        <td rowspan ="<?=count($columns);?>"><?=$table;?></td>
+                    <?php $first = false; endif;?>
+                        <td><?=$name?></td>
+                        <td><?=$details['type']?></td>
+                        <td><?=$details['length']?></td>
+                        <td></td>
+                </tr>
+          <?php endforeach;
+            endforeach;?>
+    </tbody>
+    <tbody class='created'>
+       <tr><th colspan="5">Created Tables</th></tr>
+        <?php
+            $details = Arr::get($changes, 'created_tables', array());
+            foreach ($details as $table=>$data):
+                $first = true;
+                $columns = $data['columns'];
+                foreach ($columns as $name=>$details):?>
+                <tr>
+                    <?php if ($first):?>
+                        <td rowspan ="<?=count($columns)+1;?>"><?=$table;?></td>
+                    <?php $first = false; endif;?>
+                        <td><?=$name?></td>
+                        <td><?=$details['type']?></td>
+                        <td><?=$details['length']?></td>
+                        <td></td>
+                </tr>
+          <?php endforeach;?>
+                <tr><td colspan="4"><?=Kohana::debug($data['options']);?></td></tr>
+          <?php endforeach;?>
+        <tr><th colspan="5">Created Columns</th></tr>
+        <?php
+            $details = Arr::get($changes, 'created_columns', array());
+            foreach ($details as $table=>$columns):
+                $first = true;
+                foreach ($columns as $name=>$details):?>
+                <tr>
+                    <?php if ($first):?>
+                        <td rowspan ="<?=count($columns);?>"><?=$table;?></td>
+                    <?php $first = false; endif;?>
+                        <td><?=$name?></td>
+                        <td><?=$details['type']?></td>
+                        <td><?=$details['length']?></td>
+                        <td></td>
+                </tr>
+          <?php endforeach;
+            endforeach;?>
+        <tr><th colspan="5">Created Foreign Keys</th></tr>
+        <?php
+            $details = Arr::get($changes, 'created_foreign_keys', array());
+            foreach ($details as $table=>$columns):
+                $first = true;
+                foreach ($columns as $name=>$details):?>
+                <tr>
+                    <?php if ($first):?>
+                        <td rowspan ="<?=count($columns);?>"><?=$table;?></td>
+                    <?php $first = false; endif;?>
+                        <td><?=$name?></td>
+                        <td><?=$details['local']?></td>
+                        <td><?=$details['foreign']?></td>
+                        <td><?=$details['foreignTable']?></td>
+                </tr>
+          <?php endforeach;
+            endforeach;?>
+        <tr><th colspan="5">Created Indexes</th></tr>
+        <?php
+            $details = Arr::get($changes, 'created_indexes', array());
+            foreach ($details as $table=>$columns):
+                $first = true;
+                foreach ($columns as $name=>$details):?>
+                <tr>
+                    <?php if ($first):?>
+                        <td rowspan ="<?=count($columns);?>"><?=$table;?></td>
+                    <?php $first = false; endif;?>
+                        <td><?=$name?></td>
+                        <td colspan="3"><?=implode(', ',$details['fields'])?></td>
+                </tr>
+          <?php endforeach;
+            endforeach;?>
+    </tbody>
 </table>
-<?php endforeach;?>
 <?php if ($preview):?>
     <?=Form::open()?>
     <p><?=Form::submit('build_migrations', 'Build migrations')?></p>
