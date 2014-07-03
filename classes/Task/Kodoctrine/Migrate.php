6ac05b2dbfca2796571dbb1dbedeb469ede15a4d
Andrew Coulton <andrew@ingenerator.com>
2014-07-03 14:39:05 +0100

    Make migration task non-interactive and use custom privileged creds
    The miration task should be non-interactive to run during deploy,
    and should use separate credentials to the application user (which
    should not have access to schema-altering commands).

--- classes/Task/Kodoctrine/Migrate.php
+++ classes/Task/Kodoctrine/Migrate.php
@@ -1,33 +1,57 @@
 <?php
 
 /**
- * The Migrate task runs migrations against the database schema to bring it up
- * to date.
+ * The Migrate task runs migrations against the database schema to bring it up to date.
+ * Provide a user and password with permission to modify the database schema in the --user and --password arguments.
+ *
+ * [!!] Passing credentials as arguments is not good security practice - be cautious about doing this unless you are
+ *      sure you have control of your environment.
  *
  * @package    KoDoctrine
  * @category   Administration
  * @author     Andrew Coulton
- * @copyright  (c) 2012 Andrew Coulton
+ * @copyright  (c) 2014 Andrew Coulton
  * @license    http://kohanaframework.org/license
  */
 class Task_Kodoctrine_Migrate extends Minion_Task
 {
+
+	/**
+	 * @var string[]
+	 */
+	protected $_options = array(
+		'user'     => NULL,
+		'password' => NULL,
+	);
+
+	/**
+	 * @param Validation $validation
+	 * @return Validation
+	 */
+	public function build_validation(Validation $validation)
+	{
+		$validation->rule('user',     'not_empty');
+		$validation->rule('password', 'not_empty');
+		return parent::build_validation($validation);
+	}
+
 	/**
 	 * Reconnects as an account with privileges to carry out the migration
 	 * @param string $user
 	 * @param string $password
 	 */
 	protected function elevate_db_user($user, $password) {
-        $connection = Doctrine_Manager::connection();
-        $connection->close();
-        $connection->setOption('username', $user);
-        $connection->setOption('password', $password);
-        $connection->connect();
-    }
+		$connection = Doctrine_Manager::connection();
+		$connection->close();
+		$connection->setOption('username', $user);
+		$connection->setOption('password', $password);
+		$connection->connect();
+	}
 
 	public function _execute(array $params)
 	{
 		$config = Kohana::$config->load('doctrine');
+		$this->elevate_db_user($params['user'], $params['password']);
 
 		$migration_path = $config->migration_classes;
 		Minion_CLI::write('Checking for migrations in '.Debug::path($migration_path).PHP_EOL);
@@ -44,20 +68,8 @@ class Task_Kodoctrine_Migrate extends Minion_Task
 
 		Minion_CLI::write('*********************************************************************');
 		Minion_CLI::write('Your database is at version '.$current.' and needs to be migrated to version '.$latest,
-				'white','red');
+			'white','red');
 		Minion_CLI::write('*********************************************************************'.PHP_EOL);
-
-		if (Minion_CLI::read(PHP_EOL.'This cannot be reversed - please confirm you wish to proceed',array('y','n'))!='y')
-		{
-			Minion_CLI::write('Your database has not been changed. To run migrations in future, run minion kodoctrine:migrate');
-			return;
-		}
-
-		Minion_CLI::write('Please provide credentials of a user with permission to migrate');
-		Minion_CLI::write('-database '.Doctrine_Manager::connection()->getOption('dsn'));
-		$user = Minion_CLI::read(PHP_EOL.'Username');
-		$password = Minion_CLI::read('Password');
-		$this->elevate_db_user($user, $password);
 
 		$migration->migrate();
 
